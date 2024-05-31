924ddf6831222d17ff5a83122d601ddda47113d2
jlbrt <72817409+jlbrt@users.noreply.github.com>
2024-05-31 12:23:52 +0200

    run migrations on startup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
+import path from 'node:path';
 import * as twitchBot from './twitchBot';
 import * as envValidator from './utils/envValidator';
 import * as webServer from './webserver';
 import * as cronJobs from './cronJobs';
+import { dbConnection } from './DAO/db';
 
 (async () => {
   await envValidator.validateEnvironmentVars();
+
+  await dbConnection.migrate.latest({
+    directory: path.join(__dirname, '../migrations'),
+  });
 
   await Promise.all([
     twitchBot.createAndStartBot(),
